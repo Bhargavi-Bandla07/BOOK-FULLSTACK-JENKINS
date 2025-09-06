e17731aa6eb1ff07bda9d695f1ec9d8cc78b3f3b
BHARGAVI <bhargavi07.b@gmail.com>
2025-09-06 09:48:12 +0530

    updated

--- BOOKAPI-REACT/vite.config.js
+++ BOOKAPI-REACT/vite.config.js
@@ -5,13 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/bookreactapi/',
-server: {
-    proxy: {
-      '/bookapi': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-});
+})
